0a1cded5e0551ba602e24e3c02fdf27233d3e285
Justin <94834617+JustinAhtow@users.noreply.github.com>
2025-10-16 10:10:10 +0200

    add my solution without a loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
+let str = "hey";
 
+const repeatString = function(a) {
+    return a+a+a;
 };
+
+repeatString(str);
 
 // Do not edit below this line
 module.exports = repeatString;
