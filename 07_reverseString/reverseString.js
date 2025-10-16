77997ba59144d79775fcaf35398e4545d192778b
Justin <94834617+JustinAhtow@users.noreply.github.com>
2025-10-16 11:49:14 +0200

    add correct split, reverse and join methods to pass test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 };
+
+reverseString("apple and banana");
 
 // Do not edit below this line
 module.exports = reverseString;
