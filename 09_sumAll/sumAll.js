138dc444fa20ab8a10bb8a0b01da58fa2525f117
Justin <94834617+JustinAhtow@users.noreply.github.com>
2025-10-16 13:42:33 +0200

    create a var and loop to add 2 numbers as well as numbers betweeen

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    let finalSum = 0;
+
+    for(let i = a; i <= b; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
